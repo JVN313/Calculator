230cb9d40a69e2307b0b82ff284c0c8c8b4f34ca
JVN313 <82493100+JVN313@users.noreply.github.com>
2021-10-12 14:49:11 -0400

    Improved error proofing and created cleaner code by adding lists

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 accepted_ops= ["ADDITION","ADD","ADDING","+","PLUS","SUBTRACT","MINUS","SUBTRACTION","SUBTRACTING","-","MULTIPLY","TIMES","MULTIPLICATION","X","MULTIPLYING","*","DIVISION","DIVIDE","DIVIDING","/"]
 
+addition_ops=["ADDITION","ADD","ADDING","+","PLUS"]
+subtraction_ops=["SUBTRACT","MINUS","SUBTRACTION","SUBTRACTING","-"]
+multiplication_ops=["MULTIPLY","TIMES","MULTIPLICATION","X","MULTIPLYING","*"]
+division_ops=["DIVISION","DIVIDE","DIVIDING","/"]
+
 op = input("What type of math are we doing today? ").upper()
 while op not in accepted_ops:
     print("ERROR NOT A VALID OPERATION")
@@ -8,13 +13,13 @@ while op not in accepted_ops:
 num1 = float(input("Enter your first number: "))
 num2 = float(input("Enter your second number: "))
 
-if op.upper() == "ADDITION" or op.upper() == "ADD" or op.upper() == "PLUS" or op.upper() == "ADDING" or op == "+":
+if op in addition_ops:
     print(num1 + num2)
-elif op.upper() == "SUBTRACT" or op.upper() == "MINUS" or op.upper() == "SUBTRACTION" or op.upper() == "SUBTRACTING" or op == "-":
+elif op in subtraction_ops:
     print(num1 - num2)
-elif op.upper() == "MULTIPLY" or op.upper() == "TIMES" or op.upper() == "MULTIPLICATION" or op.upper() == "X" or op.upper() == "MULTIPLYING" or op == "*":
+elif op in multiplication_ops:
     print(num1 * num2)
-elif op.upper() == "DIVISION" or op.upper() == "DIVIDE" or op.upper() == "DIVIDING" or op == "/":
+elif op in division_ops:
     if num2 == 0:
         print("ERROR Cannot Divide By 0")
     else:

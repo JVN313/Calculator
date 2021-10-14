0cc603ff8c89d75e2cfa5862572d949250f7d887
JVN313 <82493100+JVN313@users.noreply.github.com>
2021-10-14 12:47:14 -0400

    Update

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ subtraction_ops=["SUBTRACT","MINUS","SUBTRACTION","SUBTRACTING","-"]
 multiplication_ops=["MULTIPLY","TIMES","MULTIPLICATION","X","MULTIPLYING","*"]
 division_ops=["DIVISION","DIVIDE","DIVIDING","/"]
 
-op = input("What type of math are we doing today? ").upper()
-while op not in accepted_ops:
-    print("ERROR NOT A VALID OPERATION")
-    op = input("What type of math are we doing today? ").upper()
+playing_ops = ["YES","NO"]
+def main():
+        op = input("What type of math are we doing today? ").upper()
+        while op not in accepted_ops:
+            print("ERROR NOT A VALID OPERATION")
+            op = input("What type of math are we doing today? ").upper()
 
-num1 = float(input("Enter your first number: "))
-num2 = float(input("Enter your second number: "))
+        num1 = float(input("Enter your first number: "))
+        num2 = float(input("Enter your second number: "))
 
-if op in addition_ops:
-    print(num1 + num2)
-elif op in subtraction_ops:
-    print(num1 - num2)
-elif op in multiplication_ops:
-    print(num1 * num2)
-elif op in division_ops:
-    if num2 == 0:
-        print("ERROR Cannot Divide By 0")
-    else:
-        print(num1 / num2)
+        if op in addition_ops:
+            print(num1 + num2)
+        elif op in subtraction_ops:
+            print(num1 - num2)
+        elif op in multiplication_ops:
+            print(num1 * num2)
+        elif op in division_ops:
+            if num2 == 0:
+                print("ERROR Cannot Divide By 0")
+            else:
+                print(num1 / num2)
+
+main()
